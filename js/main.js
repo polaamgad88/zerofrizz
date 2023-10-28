f82f28929fd32be062ef9f6f5ca1b1b89b6b6b41
marawanFawzy <85061245+marawanFawzy@users.noreply.github.com>
2023-10-28 08:31:10 +0200

    format added category query added product page query

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@
 'use strict';
 
 
-$(window).on('load', function() {
+$(window).on('load', function () {
 	/*------------------
 		Preloder
 	--------------------*/
@@ -18,12 +18,12 @@ $(window).on('load', function() {
 
 });
 
-(function($) {
+(function ($) {
 	/*------------------
 		Navigation
 	--------------------*/
 	$('.main-menu').slicknav({
-		prependTo:'.main-navbar .container',
+		prependTo: '.main-navbar .container',
 		closedSymbol: '<i class="flaticon-right-arrow"></i>',
 		openedSymbol: '<i class="flaticon-down-arrow"></i>'
 	});
@@ -33,20 +33,20 @@ $(window).on('load', function() {
 		ScrollBar
 	--------------------*/
 	$(".cart-table-warp, .product-thumbs").niceScroll({
-		cursorborder:"",
-		cursorcolor:"#afafaf",
-		boxzoom:false
+		cursorborder: "",
+		cursorcolor: "#afafaf",
+		boxzoom: false
 	});
 
 
 	/*------------------
 		Category menu
 	--------------------*/
-	$('.category-menu > li').hover( function(e) {
+	$('.category-menu > li').hover(function (e) {
 		$(this).addClass('active');
 		e.preventDefault();
 	});
-	$('.category-menu').mouseleave( function(e) {
+	$('.category-menu').mouseleave(function (e) {
 		$('.category-menu li').removeClass('active');
 		e.preventDefault();
 	});
@@ -55,7 +55,7 @@ $(window).on('load', function() {
 	/*------------------
 		Background Set
 	--------------------*/
-	$('.set-bg').each(function() {
+	$('.set-bg').each(function () {
 		var bg = $(this).data('setbg');
 		$(this).css('background-image', 'url(' + bg + ')');
 	});
@@ -66,27 +66,27 @@ $(window).on('load', function() {
 		Hero Slider
 	--------------------*/
 	var hero_s = $(".hero-slider");
-    hero_s.owlCarousel({
-        loop: true,
-        margin: 0,
-        nav: true,
-        items: 1,
-        dots: true,
-        animateOut: 'fadeOut',
-    	animateIn: 'fadeIn',
-        navText: ['<i class="flaticon-left-arrow-1"></i>', '<i class="flaticon-right-arrow-1"></i>'],
-        smartSpeed: 1200,
-        autoHeight: false,
-        autoplay: true,
-        onInitialized: function() {
-        	var a = this.items().length;
-            $("#snh-1").html("<span>1</span><span>" + a + "</span>");
-        }
-    }).on("changed.owl.carousel", function(a) {
-        var b = --a.item.index, a = a.item.count;
-    	$("#snh-1").html("<span> "+ (1 > b ? b + a : b > a ? b - a : b) + "</span><span>" + a + "</span>");
+	hero_s.owlCarousel({
+		loop: true,
+		margin: 0,
+		nav: true,
+		items: 1,
+		dots: true,
+		animateOut: 'fadeOut',
+		animateIn: 'fadeIn',
+		navText: ['<i class="flaticon-left-arrow-1"></i>', '<i class="flaticon-right-arrow-1"></i>'],
+		smartSpeed: 1200,
+		autoHeight: false,
+		autoplay: true,
+		onInitialized: function () {
+			var a = this.items().length;
+			$("#snh-1").html("<span>1</span><span>" + a + "</span>");
+		}
+	}).on("changed.owl.carousel", function (a) {
+		var b = --a.item.index, a = a.item.count;
+		$("#snh-1").html("<span> " + (1 > b ? b + a : b > a ? b - a : b) + "</span><span>" + a + "</span>");
 
-    });
+	});
 
 	hero_s.append('<div class="slider-nav-warp"><div class="slider-nav"></div></div>');
 	$(".hero-slider .owl-nav, .hero-slider .owl-dots").appendTo('.slider-nav');
@@ -100,20 +100,20 @@ $(window).on('load', function() {
 		loop: true,
 		nav: true,
 		dots: false,
-		margin : 30,
+		margin: 30,
 		autoplay: true,
 		navText: ['<i class="flaticon-left-arrow-1"></i>', '<i class="flaticon-right-arrow-1"></i>'],
-		responsive : {
-			0 : {
+		responsive: {
+			0: {
 				items: 1,
 			},
-			480 : {
+			480: {
 				items: 2,
 			},
-			768 : {
+			768: {
 				items: 3,
 			},
-			1200 : {
+			1200: {
 				items: 4,
 			}
 		}
@@ -126,15 +126,15 @@ $(window).on('load', function() {
 	$('.popular-services-slider').owlCarousel({
 		loop: true,
 		dots: false,
-		margin : 40,
+		margin: 40,
 		autoplay: true,
-		nav:true,
-		navText:['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
-		responsive : {
-			0 : {
+		nav: true,
+		navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+		responsive: {
+			0: {
 				items: 1,
 			},
-			768 : {
+			768: {
 				items: 2,
 			},
 			991: {
@@ -182,7 +182,7 @@ $(window).on('load', function() {
 	/*-------------------
 		Quantity change
 	--------------------- */
-    var proQty = $('.pro-qty');
+	var proQty = $('.pro-qty');
 	proQty.prepend('<span class="dec qtybtn">-</span>');
 	proQty.append('<span class="inc qtybtn">+</span>');
 	proQty.on('click', '.qtybtn', function () {
@@ -206,15 +206,12 @@ $(window).on('load', function() {
 	/*------------------
 		Single Product
 	--------------------*/
-	$('.product-thumbs-track > .pt').on('click', function(){
+	$('.product-thumbs-track > .pt').on('click', function () {
 		$('.product-thumbs-track .pt').removeClass('active');
 		$(this).addClass('active');
 		var imgurl = $(this).data('imgbigurl');
-		var bigImg = $('.product-big-img').attr('src');
-		if(imgurl != bigImg) {
-			$('.product-big-img').attr({src: imgurl});
-			$('.zoomImg').attr({src: imgurl});
-		}
+		$('.product-big-img').attr({ src: imgurl });
+		$('.zoomImg').attr({ src: imgurl, crossorigin: 'anonymous' });
 	});
 
 
